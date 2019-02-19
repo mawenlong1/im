753ed36b49af061cb7c4aa679aadce31e892a6f8
mawenlong <15061886113@163.com>
2019-02-19 16:07:20 +0800

    客户端发送消息与服务器端响应消息

---
+++ src/main/java/com/mwl/im/protocol/response/MessageResponsePacket.java
@@ -0,0 +1,20 @@
+package com.mwl.im.protocol.response;
+
+import com.mwl.im.protocol.Packet;
+import com.mwl.im.protocol.command.Command;
+import lombok.Data;
+
+/**
+ * @author mawenlong
+ * @date 2019/02/19
+ */
+@Data
+public class MessageResponsePacket extends Packet {
+
+    private String message;
+
+    @Override
+    public Byte getCommand() {
+        return Command.MESSAGE_RESPONSE;
+    }
+}
