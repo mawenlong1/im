9121c79eac2e7e53ef6ae6caa3c2b07d9f8c0a4d
mawenlong <15061886113@163.com>
2019-02-19 15:52:15 +0800

    登录功能

--- src/main/java/com/mwl/im/client/Client.java
+++ src/main/java/com/mwl/im/client/Client.java
@@ -7,6 +7,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
@@ -27,9 +28,13 @@ public class Client {
         Bootstrap b = new Bootstrap();
         b.group(group)
          .channel(NioSocketChannel.class)
+         // 表示连接的超时时间，超过这个时间还是建立不上的话则代表连接失败
          .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+         // 表示是否开启 TCP 底层心跳机制，true 为开启
          .option(ChannelOption.SO_KEEPALIVE, true)
+         // 表示是否开始 Nagle 算法，true 表示关闭，false 表示开启，通俗地说，如果要求高实时性，有数据发送时就马上发送，就设置为 true 关闭，如果需要减少发送次数减少网络交互，就设置为 false 开启
          .option(ChannelOption.TCP_NODELAY, true)
+         .attr(AttributeKey.newInstance("clientName"), "IM Client")
          .handler(new ChannelInitializer<SocketChannel>() {
              @Override
              protected void initChannel(SocketChannel ch) throws Exception {
