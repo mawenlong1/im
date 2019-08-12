ae09674bb67778f20a2fc3287b25e031e6c5e90c
mawenlong <mwljndx@163.com>
2019-08-12 11:25:58 +0800

    fix

--- src/main/java/com/mwl/im/client/Client.java
+++ src/main/java/com/mwl/im/client/Client.java
@@ -49,9 +49,9 @@ public class Client {
          .channel(NioSocketChannel.class)
          // 表示连接的超时时间，超过这个时间还是建立不上的话则代表连接失败
          .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-         // 表示是否开启 TCP 底层心跳机制，true 为开启
+         // 表示是否开启TCP底层心跳机制，true为开启
          .option(ChannelOption.SO_KEEPALIVE, true)
-         // 表示是否开始 Nagle 算法，true 表示关闭，false 表示开启，通俗地说，如果要求高实时性，有数据发送时就马上发送，就设置为 true 关闭，如果需要减少发送次数减少网络交互，就设置为 false 开启
+         // 表示是否开始Nagle算法，true 表示关闭，false 表示开启，通俗地说，如果要求高实时性，有数据发送时就马上发送，就设置为 true 关闭，如果需要减少发送次数减少网络交互，就设置为 false 开启
          .option(ChannelOption.TCP_NODELAY, true)
          .attr(AttributeKey.newInstance("clientName"), "IM Client")
          .handler(new ChannelInitializer<SocketChannel>() {
@@ -111,7 +111,6 @@ public class Client {
     private static void startConsolThread(Channel channel) {
         ConsoleCommandManager manager = new ConsoleCommandManager();
         LoginConsoleCommand loginConsoleCommand = new LoginConsoleCommand();
-
         Scanner scanner = new Scanner(System.in);
         new Thread(() -> {
             while (!Thread.interrupted()) {
