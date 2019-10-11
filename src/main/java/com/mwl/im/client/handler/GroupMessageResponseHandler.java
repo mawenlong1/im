a1e7c13dfec38d072e33a64a6d7699abd12251e1
mawenlong <mwljndx@163.com>
2019-10-11 19:21:15 +0800

    fix

--- src/main/java/com/mwl/im/client/handler/GroupMessageResponseHandler.java
+++ src/main/java/com/mwl/im/client/handler/GroupMessageResponseHandler.java
@@ -12,8 +12,6 @@ import io.netty.channel.SimpleChannelInboundHandler;
 public class GroupMessageResponseHandler extends SimpleChannelInboundHandler<GroupMessageResponsePacket> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, GroupMessageResponsePacket msg) throws Exception {
-        String fromGroupId = msg.getFromGroupId();
-        Session fromUser = msg.getFromUser();
-        System.out.println("收到群[" + fromGroupId + "]中[" + fromUser + "]发来的消息：" + msg.getMessage());
+        System.out.println("收到群[" + msg.getFromGroupId() + "]中[" + msg.getFromUser() + "]发来的消息：" + msg.getMessage());
     }
 }
